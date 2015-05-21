66caea147b836de9a8b3cf5f98f888408632193f
xiongjiji <898375292@qq.con>
2015-05-21 17:58:42 +0800

    第一版本完成

--- service.php
+++ service.php
@@ -9,10 +9,11 @@ header("Content-Type: text/plain;charset=utf-8");
 //定义一个多维数组，包含产品的信息，每条信息为一个数组
 $staff = array
 	(
-		array("name" => "炒饭", "number" => "1", "price" => "2", "amount" => "份","people" =>"3","img" =>"./image/logo.png","id" => "1341515"),
-		array("name" => "炒饭", "number" => "1", "price" => "2", "amount" => "份","people" =>"3","img" =>"./image/logo.png","id" => "1341515"),
-		array("name" => "炒饭", "number" => "1", "price" => "2", "amount" => "份","people" =>"3","img" =>"./image/logo.png","id" => "1341516"),
-		array("name" => "炒菜", "number" => "2", "price" => "4", "amount" => "份","people" =>"33","img" =>"./image/logo.png","id" => "1234"),
+		array("name" => "炒饭", "number" => "1", "price" => "2", "amount" => "份","people" =>"3","img" =>"./image/logo.png","id" => "111114"),
+		array("name" => "广州炒饭", "number" => "1", "price" => "2", "amount" => "份","people" =>"3","img" =>"./image/logo.png","id" => "111112"),
+		array("name" => "印度炒饭", "number" => "1", "price" => "2", "amount" => "份","people" =>"3","img" =>"./image/logo.png","id" => "111113"),
+		array("name" => "炒菜", "number" => "2", "price" => "4", "amount" => "份","people" =>"33","img" =>"./image/logo.png","id" => "211111"),
+		array("name" => "广州炒菜", "number" => "2", "price" => "4", "amount" => "份","people" =>"33","img" =>"./image/logo.png","id" => "211112"),
 		array("name" => "粥", "number" => "3", "price" => "5", "amount" => "碗","people" =>"34","img" =>"./image/logo.png","id" => "3414"),
 		array("name" => "肠粉", "number" => "4", "price" => "5", "amount" => "份","people" =>"35","img" =>"./image/logo.png","id" => "3422224"),
 		array("name" => "面包", "number" => "5", "price" => "6", "amount" => "个","people" =>"31","img" =>"./image/logo.png","id" => "342344414"),
